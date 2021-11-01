ec4a7973cffa909a04d0244e3dbf5a6c5641878c
iXploitID <93501879+iXploitID@users.noreply.github.com>
2021-11-01 10:45:23 +0700

    Add files via upload

---
+++ sc2.sh
@@ -0,0 +1,89 @@
+echo -e " [#] nama file  ngga usah ada tulisa (.html) "
+read -p ' [@] Nama file   : ' nama
+echo
+echo -e " [@] Buat Upload foto/lagu"
+echo -e " [@] link :  https://top4top.io/"
+echo
+read -p ' [#] Link Gambar [$]: ' gambar
+read -p ' [#] Hacked by   [$]: ' hack
+read -p ' [#] Pesan       [$]: ' pes
+read -p ' [#] Thanks to   [$]: ' tank
+read -p ' [#] Link lagu   [$]: ' lagu
+
+
+
+cd $HOME/storage/downloads                            
+cat <<LOGIN>$nama.html
+
+
+
+<!doctype html>
+<html>
+<head>
+<title>$hack</title>
+<linkhref='https://fonts.googleapis.com/css?family=Jolly Lodger' rel='stylesheet'>
+<script type="text/javascript">     
+{
+}</script>
+
+<center><audio autoplay="autoplay" controls="controls" src="$lagu"></audio></a></center>
+ 
+<center><br>
+
+
+
+</style>
+
+ <meta charset="utf-8">
+   <meta content="# Gua squad anjink#" name="description"/>
+   <meta content="[ kalian semua]" name="keyword"/>
+  <style type="text/css">
+		
+	body {
+<style
+background-image:url('');
+				background-color: #000000;
+				background-repeat:no-repeat;
+				background-size: 100% ;
+				background-position:top center;
+		
+}</style>	
+	  <body onUnload="UserExit()">
+	  <script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js"></script>
+
+     </head>
+
+<script language=JavaScript> message="Anti Copas Njenk!!!";function clickIE4(){if (event.button==2){alert(message);return false;}}function clickNS4(e){if (document.layers||document.getElementById&&!document.all){if (e.which==2||e.which==3){alert(message);return false;}}}if (document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;}else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;}document.oncontextmenu=new Function("alert(message);return false")// --></script>
+         &nbsp;     
+           <center><img src="$gambar"width="600"height="600"></center></body>
+     <center><img src="http://www6.0zz0.com/2011/03/14/06/269205957.gif" id="button1" name="button1" border="0" height="7" width="700"></center> 
+  
+             <center><font face="Jolly Lodger" size="9" style="color:#ffffff;text-shadow:#FF0099 0px 0px 10px">Hacked by $hack</font></center>
+
+<center><marquee behavior="scroll" direction="left" scrollamount="130" scrolldelay="40" width="100%">
+
+<font 
+
+color="white" size="3">______________________________________________________________</font></marquee></center>
+
+<center><marquee behavior="scroll" direction="right" scrollamount="130" scrolldelay="40" width="100%">
+
+<font 
+
+color="red" size="3">______________________________________________________________</font></marquee></center>        
+             <center><font face="Jolly Lodger" size="9" color="cyan"> <center><font face="Jolly Lodger" size="7"><br>$pes<br>
+<center><font face="Jolly Lodger" color="red" size="5">Thanks To : </font><marquee behavior="scroll" direction="left" scrollamount="17" width="70%">
+
+<font 
+face="Forte"
+color="lime" size="5">-=|$tank|=-
+</font></marquee></center>
+
+
+</table>
+</body>
+ <html>
+LOGIN
+cp -f $nama.html /sdcard
+echo
+echo -e " [✓] Nama Sc $nama.html | Tersimpan di downloads"
---
+++ simpel.sh
@@ -0,0 +1,126 @@
+clear
+
+
+
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+
+
+
+blue='\e[0;34'p
+cyan='\e[0;36m'
+green='\e[0;34m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+#JANGAN DI RECROD MAK :V CAPE BUAT NYA :V
+read -p "Nama mu.? [] : " cc;
+clear
+toilet -f mono12 -F metal "HALO"|lolcat
+toilet -f mono12 -F metal "$cc"|lolcat
+toilet -f mono12 -F metal "Suscibe"|lolcat
+toilet -f mono12 -F metal "chenell"|lolcat
+toilet -f mono12 -F metal "gw ya"|lolcat
+sleep 2
+clear
+echo -e "CODED BY :"
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+echo
+echo -e $red "################################"
+echo -e $red "# CREATOR: i-Xploit            #"
+echo -e $red "# Yt: Ramdom Toturial          #"
+echo -e $white "#jangan recrod script ini     #"
+echo -e $white "###############################"
+echo -e $red"▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
+echo -e $red"████████████████████████████████████████"
+echo -e $red"████████████████████████████████████████"
+echo -e $red"████████████████████████████████████████"
+echo -e $red"████████████████████████████████████████"
+echo -e $white"████████████████████████████████████████"
+echo -e $white"████████████████████████████████████████"
+echo -e $white"████████████████████████████████████████"
+echo -e $white"████████████████████████████████████████"
+
+echo
+echo
+echo
+echo -e "1.SC DEFACE KE 1"|lolcat
+echo -e "2.SC DEFAFE KE 2"|lolcat
+echo -e "3.SC DEFACE KE 3"|lolcat
+read -p "[MASUAKAN INPUT NOMER]> " a;
+
+if [ $a = 1 ] || [ $a = 1 ]
+
+
+then
+clear
+cd $HOME/SIMPELSC
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+echo -e "CODED BY :"
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+
+
+bash sc1.sh
+fi
+
+
+
+if [ $a = 2 ] || [ $a = 2 ]
+then
+clear
+cd $HOME/SIMPELSC
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+echo -e "CODED BY :"
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+
+
+bash sc2.sh
+fi
+
+
+if [ $a = 3 ] || [ $a = 3 ]
+then
+clear
+cd $HOME/SIMPELSC
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+echo -e "CODED BY :"
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+bash sc3.sh
+fi
